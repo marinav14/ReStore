0aeeffec61309b71b639444750581fe5c94f3b4b
marinav14 <127361672+marinav14@users.noreply.github.com>
2023-03-11 02:10:15 +0100

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
-import Catalog from "../../features/catalog/catalog";
+import { Outlet } from "react-router-dom";
 import Header from "./Header";
 
 
@@ -25,7 +25,7 @@ function handleThemeChange(){
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Outlet />
       </Container>
     </ThemeProvider>
   );
